7de917d18e63af3da7d237c2508c86b488987d90
your name <name@example.com>
2023-04-25 00:12:21 +0400

    fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,17 +15,16 @@ public class Main {
             boolean validNum = false;
             int counter = 1;
             while (counter < 6) {
-                do {
                     try {
                         System.out.println("please enter number #" + counter);
                         int num = Integer.parseInt(scanner.nextLine());
-                        validNum = num < 0 ? false : true;
-                        summ+=num;
-                        counter ++;
+                        if (num > 0) {
+                            summ += num;
+                            counter++;
+                        }
                     } catch (NumberFormatException badUserData) {
                         System.out.println("Characters not allowed!!! Try again.");
                     }
-                } while (!validNum);
             }
 
 
